6c737bef1137f9725a4db53049d6bd0f3da65b9c
Werka055 <161338122+Werka055@users.noreply.github.com>
2024-05-14 15:31:25 +0200

    Create

---
+++ index.js
@@ -0,0 +1,19 @@
+onst { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world from GraphQL!'
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
